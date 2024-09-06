ed4593bedcaf0eba2ce9b076b480b4d38a409e3f
TonThanyathon <tonkingrat@gmail.com>
2024-09-06 21:18:01 +0700

    SaaS Landing Page

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import Image from "next/image";
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import { motion } from "framer-motion";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +19,42 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container mx-auto">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world's most innovative teams
+        </h2>
+        <div className="relative overflow-hidden mt-9 before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
+          <motion.div
+            transition={{
+              duration: 20,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            className="flex gap-16 flex-none pr-16"
+          >
+            {images.map(({ src, alt }, index) => (
+              <Image
+                key={index}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+            {images.map(({ src, alt }, index) => (
+              <Image
+                key={index}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
